a8e928040d923e79fbb94149174715194bb32ba7
xan <47989552+style-x@users.noreply.github.com>
2022-01-28 20:02:43 +0100

    Erstes Design
    Buttons und Ausgabefelder erstellt.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+const eingabe = document.getElementById("eingabe");
+
+function add() {
+  console.log("add");
+};
+
+function subtract() {
+  console.log("subtract");
+};
+
+function multiply() {
+  console.log("multiply");
+};
+
+function divide() {
+  console.log("divide");
+};
+
+function operate() {
+  console.log("operate");
+};
+
