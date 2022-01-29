ddffa0263d35b37a6e4a1a383cc992b9fc2ec782
xan <47989552+style-x@users.noreply.github.com>
2022-01-29 20:00:01 +0100

    Style Anpassung, JS Anfang
    Nun müssen die Funktionen in JS geschrieben werden..

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
+const btnNr = document.querySelectorAll("[data-nr]");
+const btnOp = document.querySelectorAll("[data-op]");
+const btnEqual = document.querySelectorAll("[data-equals]");
+const btnDel = document.querySelectorAll("[data-del]");
+const btnAc = document.querySelectorAll("[data-ac]");
+const txtPrevious = document.querySelectorAll("[data-previous]");
+const txtCurrent = document.querySelectorAll("[data-current]");
 
-const eingabe = document.getElementById("eingabe");
+
 
 function add() {
   console.log("add");
@@ -21,3 +28,16 @@ function operate() {
   console.log("operate");
 };
 
+function back() {
+  console.log("delete");
+};
+
+function clear() {
+  console.log("clear");
+};
+
+function append(nr) {
+  if (nr === ".") {
+    
+  }
+};
